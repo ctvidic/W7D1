165ac3cb6061e10d22d91e1d0f7253730a9fa7b5
James <jameschen625@gmail.com>
2021-07-26 15:01:31 -0400

    started users

---
+++ skeleton/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+
+end
---
+++ skeleton/db/migrate/20210726181918_create_edit.rb
@@ -0,0 +1,5 @@
+class CreateEdit < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :username, unique: true
+  end
+end
