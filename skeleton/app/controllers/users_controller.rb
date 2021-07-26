7b2d37da7ae7a277daad54b7452f685a15384ffe
ctvidic <chrisvidic.cv@gmail.com>
2021-07-26 16:06:07 -0400

    Fasdfas

--- skeleton/app/controllers/users_controller.rb
+++ skeleton/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in?, redirect_to cats_url
+    before_action :redirect, except:[:new, :create]
 
     def index
         @users = User.all
